c61a91a7521358bd82d5e0c14777ae162fba3556
Qian_shuang <1969154690@qq.com>
2024-11-19 02:55:41 +0800

    补充reinit.bash

--- reinit.bash
+++ reinit.bash
@@ -1,1 +1,6 @@
-cd bilibili_project && rm -rf ./audio_file && mkdir audio_file
+echo "clear all result and reinit"
+rm -rf ./audio_file && mkdir audio_file
+rm -rf ./video_file && mkdir video_file
+rm -rf ./html_file && mkdir html_file
+rm -rf ./picture && mkdir picture
+rm -rf ./video_result && mkdir video_result
